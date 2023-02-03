7d0269831a9fd6af3a6bd358acd3241f2111e6cc
Lindsey Dew <lindsey@emitwise.com>
2023-02-03 16:40:53 +0000

    WIP addition solver

--- src/solver/Solver.test.ts
+++ src/solver/Solver.test.ts
@@ -1,19 +1,84 @@
 import { it, describe, expect } from "vitest";
-import { Solver } from "./Solver";
+import { Literal, Operation, Solver } from "./Solver";
 
 describe("Solver", () => {
-  it("should be able to solve with addition", () => {
+  it("sortDesc should put the numbers high to low", () => {
     const solver = new Solver(21, [1, 2, 3, 4, 5, 6]);
-    expect(solver.solve()).toBe("1 + 2 + 3 + 4 + 5 + 6");
+    expect(solver.sortedValues).toBe([6, 5, 4, 3, 2, 1]);
+  });
+  it.only("should be able to solve with addition", () => {
+    const solver = new Solver(6, [1, 2, 3]);
+    console.log("&*&*");
+    console.log(solver.solve());
+    console.log("&*&*");
+    expect(solver.solve()).toStrictEqual([
+      new Operation(
+        new Operation(new Literal(3), new Literal(2), "+"),
+        new Literal(1),
+        "+"
+      ),
+      new Operation(
+        new Operation(new Literal(3), new Literal(1), "+"),
+        new Literal(2),
+        "+"
+      ),
+      // new Operation(
+      //   new Operation(new Literal(2), new Literal(1), "+"),
+      //   new Literal(2),
+      //   "+"
+      // ),
+    ]);
   });
 
-  it("should stop computing if a solution is found", () => {
-    const solver = new Solver(10, [1, 2, 3, 4, 5, 6]);
-    expect(solver.solve()).toBe("1 + 2 + 3 + 4");
+  it("should only show valid solutions", () => {
+    const solver = new Solver(4, [1, 2, 3]);
+    expect(solver.solve()).toStrictEqual([
+      new Operation(new Literal(3), new Literal(1), "+"),
+    ]);
   });
 
-  it("should return undefined if no solution", () => {
-    const solver = new Solver(22, [1, 2, 3, 4, 5, 6]);
-    expect(solver.solve()).toBe(undefined);
+  // it("should stop computing if a solution is found", () => {
+  //   const solver = new Solver(10, [1, 2, 3, 4, 5, 6]);
+  //   expect(solver.solve()).toBe("1 + 2 + 3 + 4");
+  // });
+
+  // it("should return undefined if no solution", () => {
+  //   const solver = new Solver(22, [1, 2, 3, 4, 5, 6]);
+  //   expect(solver.solve()).toBe(undefined);
+  // });
+});
+
+describe("Expression", () => {
+  describe("prettyPrint", () => {
+    it("addition with two numbers", () => {
+      expect(
+        new Operation(new Literal(1), new Literal(2), "+").prettyPrint
+      ).toBe("1 + 2");
+    });
+
+    it("addition with a nested expression", () => {
+      expect(
+        new Operation(
+          new Operation(new Literal(1), new Literal(2), "+"),
+          new Literal(3),
+          "+"
+        ).prettyPrint
+      ).toBe("(1 + 2) + 3");
+    });
+  });
+
+  describe("value", () => {
+    it("addition with two numbers", () => {
+      expect(new Operation(new Literal(1), new Literal(2), "+").value).toBe(3);
+    });
+    it("addition with a nested expression", () => {
+      expect(
+        new Operation(
+          new Operation(new Literal(1), new Literal(2), "+"),
+          new Literal(3),
+          "+"
+        ).value
+      ).toBe(6);
+    });
   });
 });
--- src/solver/Solver.ts
+++ src/solver/Solver.ts
@@ -1,23 +1,104 @@
 export class Solver {
-  constructor(private target: number, private values: number[]) {}
-
-  solve() {
-    let solutionBuilder = "";
-    let currentTotal = 0;
-    this.values.forEach((v) => {
-      if (currentTotal === this.target) {
-        return solutionBuilder;
-      } else {
-        currentTotal += v;
-        solutionBuilder += `${v} + `;
-      }
-    });
-    if (currentTotal !== this.target) {
+  sortedValues: number[];
+  constructor(readonly target: number, readonly values: number[]) {
+    this.sortedValues = values.sort().reverse();
+  }
+  solverRec(
+    restOfValues: number[],
+    operation: Operation
+  ): Expression | undefined {
+    if (operation.value === this.target) {
+      return operation;
+    } else if (restOfValues.length === 0) {
       return undefined;
-    } else return solutionBuilder;
+    } else {
+      return this.solverRec(
+        restOfValues.splice(1),
+        new Operation(operation, new Literal(restOfValues[0]), "+")
+      );
+    }
+  }
+  solve(): Expression[] {
+    const startValue = this.sortedValues[0];
+
+    const solutions: (Expression | undefined)[] = this.sortedValues.map(
+      (v, idx) => {
+        if (idx === 0) {
+          return undefined;
+        } else {
+          return this.solverRec(
+            this.sortedValues.filter((_, i) => i === idx),
+            new Operation(
+              new Literal(startValue),
+              new Literal(this.sortedValues[v]),
+              "+"
+            )
+          );
+        }
+      }
+    );
+
+    return solutions.reduce<Expression[]>((prev, current) => {
+      if (current !== undefined) {
+        return [...prev, current];
+      } else return prev;
+    }, []);
   }
 }
+
+// class Expression {
+//   constructor(a: number, b: number, operation: string) {}
+// }
+
+// interface literal -> string and numeric value
+
+// two implementations -> one literal
+// an operation -> accepts expressions
+// toString,
 
 // class Solution {
 //     constructor()
 // }
+
+export interface Expression {
+  prettyPrint: string;
+  value: number;
+  valid: boolean;
+}
+
+export function isOperationType(value: Expression): value is Operation {
+  return "operation" in value;
+}
+
+export function isLiteralType(value: Expression): value is Literal {
+  return !("operation" in value);
+}
+export class Literal implements Expression {
+  prettyPrint: string;
+  valid: boolean;
+  constructor(readonly value: number) {
+    this.prettyPrint = `${value}`;
+    this.valid = true;
+  }
+}
+
+export class Operation implements Expression {
+  prettyPrint: string;
+  value: number;
+  valid: boolean;
+  operation: string;
+  constructor(
+    readonly exp1: Expression,
+    readonly exp2: Expression,
+    // TODO to make an enum / union type of these
+    operation: string
+  ) {
+    this.value = exp1.value + exp2.value;
+    this.valid = true;
+    const expToString = (exp: Expression) => {
+      return isOperationType(exp) ? `(${exp.prettyPrint})` : `${exp.value}`;
+    };
+    this.prettyPrint = `${expToString(exp1)} + ${expToString(exp2)}`;
+    this.operation = operation;
+  }
+}
