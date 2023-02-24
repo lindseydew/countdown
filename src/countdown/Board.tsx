b3619b612f483ad69fce10840c195e33e8bf5749
Lindsey Dew <lindsey@emitwise.com>
2023-02-24 15:37:39 +0000

    Use a game state enum to model states of game

--- src/countdown/Board.tsx
+++ src/countdown/Board.tsx
@@ -25,23 +25,26 @@ const removeElementFromArray: (n: number, values: number[]) => number[] = (
   const index = values.lastIndexOf(n);
   return values.filter((_, idx) => idx !== index);
 };
+
+enum GameState {
+  setUp,
+  readyToPlay,
+  playing,
+  finished,
+}
 export function Board() {
   const [largeValues, setLargeValues] = useState<number[]>(LARGE_VALUES);
   const [littleValues, setLittleValues] = useState<number[]>(
     shuffledArray(LITTLE_VALUES)
   );
   const [selectedValues, setSelectedValues] = useState<number[]>([]);
-  const [canStartGame, setCanStartGame] = useState<boolean>(false);
-  const [gameHasStarted, setGameHasStarted] = useState<boolean>(false);
   const [target, setTarget] = useState<number | undefined>(undefined);
-  const [cardFaceDown, setCardFaceDown] = useState<boolean>(true);
+  const [gameState, setGameState] = useState<GameState>(GameState.setUp);
   const [solutions, setSolutions] = useState<string[]>([]);
 
   const onStartGameClick: () => void = () => {
     setTarget(generateTarget());
-    setCardFaceDown(false);
-    setCanStartGame(false);
-    setGameHasStarted(true);
+    setGameState(GameState.playing);
   };
 
   const onShowSolutionsClick: () => void = () => {
@@ -51,18 +54,21 @@ export function Board() {
       const solutions = expressions.map((s) => s.prettyPrint);
       setSolutions(solutions);
     }
+    setGameState(GameState.finished);
   };
   const cardDroppedCallback: (n: number) => void = (n) => {
-    if (isLittleValue(n)) {
-      setLittleValues((values) => removeElementFromArray(n, values));
-      setSelectedValues((values) => [...values, n]);
-    } else {
-      setLargeValues((values) => removeElementFromArray(n, values));
-      setSelectedValues((values) => [...values, n]);
-    }
-    // TODO -> should this be a callback?
-    if ([...largeValues, ...littleValues].length <= 19) {
-      setCanStartGame(true);
+    if (gameState === GameState.setUp) {
+      if (isLittleValue(n)) {
+        setLittleValues((values) => removeElementFromArray(n, values));
+        setSelectedValues((values) => [...values, n]);
+      } else {
+        setLargeValues((values) => removeElementFromArray(n, values));
+        setSelectedValues((values) => [...values, n]);
+      }
+      // TODO -> should this be a callback?
+      if ([...largeValues, ...littleValues].length <= 19) {
+        setGameState(GameState.readyToPlay);
+      }
     }
   };
   return (
@@ -71,14 +77,16 @@ export function Board() {
         <Target value={target} />
         <CardList
           onCardDropped={cardDroppedCallback}
-          cardFaceDown={cardFaceDown}
+          cardFaceDown={
+            gameState === GameState.setUp || gameState === GameState.readyToPlay
+          }
         />
         <StartGame
-          canStartGame={canStartGame}
+          canStartGame={gameState === GameState.readyToPlay}
           onStartGameClick={onStartGameClick}
         />
         <ShowSolutionsButton
-          gameHasStarted={gameHasStarted}
+          gameHasStarted={gameState === GameState.playing}
           onShowSolutionsClick={onShowSolutionsClick}
         />
       </>
